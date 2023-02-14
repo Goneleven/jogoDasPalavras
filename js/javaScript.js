af2e3dc39a54406c6a9442b8f7ab8140a99e8b2e
GuiGui <109730367+BoneShai@users.noreply.github.com>
2023-02-14 09:27:27 -0300

    Add files via upload

---
+++ js/javaScript.js
@@ -0,0 +1,14 @@
+//ESTRUTURA DE REPETIÇÃO:
+
+
+var palavra = prompt("Escreva uma palavra para o jogo funcionar")
+
+document.write("<p>" + "A Hora do " + "<b>" + palavra + "</b>" + "<br>")
+
+document.write("O " + "<b>" + palavra + "</b>" + " De OZ" + "<br>")
+
+document.write("11 homens e um " + "<b>" + palavra + "</b>" + "<br>")
+
+document.write("<b>" + palavra + "</b>" + " Dos Macacos" + "<br>")
+
+document.write("Procurando " + "<b>" + palavra + "</b>" + "<br>" + "</p>")
